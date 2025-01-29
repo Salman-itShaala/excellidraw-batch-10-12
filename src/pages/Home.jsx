89aaa7936e77ded5d8fe72cdf86f157f66000538
salman-itShaala <salman.shaikh@itshaala.com>
2025-01-29 14:49:21 +0530

    able to draw rect shape todo: show it while drawing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Stage, Layer, Rect, Circle, TextPath, Text } from "react-konva";
 
 const Home = () => {
   const [shapes, setShapes] = useState([{}]);
 
-  /*
-    
-    {
-    "id": "T5-srFFohXmZMaagnYGJb",
-    "type": "rectangle",
-    "x": 20851.096308390297,
-    "y": 72849.8097795759,
-    "width": 3326.6668701171875,
-    "height": 3766.6668701171875,
-    "angle": 0,
-    "strokeColor": "#2f9e44",
-    "backgroundColor": "#b2f2bb",
-    "strokeWidth": 1,
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      let canvasEl = canvasRef.current;
+      let x = 0;
+      let y = 0;
+      let height = 0;
+      let width = 0;
+
+      //   canvasEl.addEventListener("mousemove", (e) => {
+      //     console.log("X axis is ", e.clientX);
+      //     console.log("Y axis is ", e.clientY);
+      //   });
+
+      canvasEl.addEventListener("mousedown", (e) => {
+        x = e.clientX;
+        y = e.clientY;
+      });
+
+      canvasEl.addEventListener("mouseup", (e) => {
+        width = e.clientX - x;
+        height = e.clientY - y;
+
+        console.log("x is ", x, "y is ", y);
+        console.log("height is ", height, "widht is ", width);
+
+        let shape = {
+          x: x,
+          y: y,
+          width: width,
+          height: height,
+          fill: "red",
+          type: "rect",
+          id: Date.now(),
+        };
+
+        setShapes((p) => [...p, shape]);
+      });
+
+      return () => {
+        canvasEl.removeEventListener("mouseup");
+        canvasEl.removeEventListener("mousedown");
+        canvasEl.removeEventListener("mousemove");
+      };
     }
-    */
+  }, []);
 
   function drawRect() {
     // {x, y, width, height, fill, type}
@@ -52,6 +84,7 @@ const Home = () => {
             className="bg-slate-800 min-h-screen text-slate-100"
             width={window.innerWidth}
             height={window.innerHeight}
+            ref={canvasRef}
           >
             <Layer>
               {shapes.map((shape) => {
