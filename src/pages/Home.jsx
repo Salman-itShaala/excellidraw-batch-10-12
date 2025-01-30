c79f5d146aab3ef7a2af4ed9cfcf97b2f17d56fd
salman-itShaala <salman.shaikh@itshaala.com>
2025-01-30 13:47:24 +0530

    able to draw and see elements drawing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,8 @@ const Home = () => {
 
   const canvasRef = useRef(null);
 
+  let idRef = useRef(null);
+
   useEffect(() => {
     if (canvasRef.current) {
       let canvasEl = canvasRef.current;
@@ -13,35 +15,49 @@ const Home = () => {
       let y = 0;
       let height = 0;
       let width = 0;
-
-      //   canvasEl.addEventListener("mousemove", (e) => {
-      //     console.log("X axis is ", e.clientX);
-      //     console.log("Y axis is ", e.clientY);
-      //   });
+      let isDrawing = false;
 
       canvasEl.addEventListener("mousedown", (e) => {
+        isDrawing = true;
         x = e.clientX;
         y = e.clientY;
-      });
 
-      canvasEl.addEventListener("mouseup", (e) => {
-        width = e.clientX - x;
-        height = e.clientY - y;
-
-        console.log("x is ", x, "y is ", y);
-        console.log("height is ", height, "widht is ", width);
+        idRef.current = Date.now();
 
         let shape = {
           x: x,
           y: y,
-          width: width,
-          height: height,
-          fill: "red",
+          width: 0,
+          height: 0,
           type: "rect",
-          id: Date.now(),
+          fill: "red",
+          stroke: "white",
+          strokeWidth: 30,
+          opacity: 0.5,
+          id: idRef.current,
         };
 
         setShapes((p) => [...p, shape]);
+      });
+
+      canvasEl.addEventListener("mousemove", (e) => {
+        if (!isDrawing) return;
+
+        setShapes((shapes) => {
+          return shapes.map((shape) => {
+            if (shape.id === idRef.current) {
+              shape.height = e.clientY - y;
+              shape.width = e.clientX - x;
+            }
+            return shape;
+          });
+        });
+      });
+
+      canvasEl.addEventListener("mouseup", (e) => {
+        isDrawing = false;
+        width = e.clientX - x;
+        height = e.clientY - y;
       });
 
       return () => {
@@ -52,59 +68,35 @@ const Home = () => {
     }
   }, []);
 
-  function drawRect() {
-    // {x, y, width, height, fill, type}
-
-    let shape = {
-      x: 200,
-      y: 200,
-      width: 200,
-      height: 200,
-      fill: "red",
-      type: "rect",
-      id: Date.now(),
-    };
-
-    setShapes((p) => [...p, shape]);
-  }
-
   return (
     <>
-      {shapes.length === 0 ? (
-        <div className="bg-slate-800 min-h-screen text-slate-100">
-          <p className="text-center text-4xl font-bold">
-            Welcome to Excellidraw
-          </p>
-          <p className="text-center">Worlds #1 note making/drawing app</p>
-        </div>
-      ) : (
-        <>
-          <button onClick={drawRect}>Add react</button>
-          <Stage
-            className="bg-slate-800 min-h-screen text-slate-100"
-            width={window.innerWidth}
-            height={window.innerHeight}
-            ref={canvasRef}
-          >
-            <Layer>
-              {shapes.map((shape) => {
-                if (shape.type === "rect") {
-                  return (
-                    <Rect
-                      key={shape.id}
-                      x={shape.x}
-                      y={shape.y}
-                      width={shape.width}
-                      height={shape.height}
-                      fill={shape.fill}
-                    />
-                  );
-                }
-              })}
-            </Layer>
-          </Stage>
-        </>
-      )}
+      <Stage
+        className="bg-slate-800 min-h-screen text-slate-100"
+        width={window.innerWidth}
+        height={window.innerHeight}
+        ref={canvasRef}
+      >
+        <Layer>
+          {shapes.map((shape) => {
+            if (shape.type === "rect") {
+              return (
+                <Rect
+                  key={shape.id}
+                  x={shape.x}
+                  y={shape.y}
+                  width={shape.width}
+                  height={shape.height}
+                  fill={shape.fill}
+                  draggable
+                  strokeWidth={5}
+                  opacity={0.8}
+                  stroke={"white"}
+                />
+              );
+            }
+          })}
+        </Layer>
+      </Stage>
     </>
   );
 };
